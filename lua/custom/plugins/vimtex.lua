c6620f957d688f11ccf7805980db0fab9266ddfb
Javier Acevedo <ja.acevedo12@uniandes.edu.co>
2024-10-14 02:14:43 +0159

    It kind of works now

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -4,6 +4,10 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = 'zathura'
+    -- vim.g.vimtex_view_method = 'evince'
+    -- vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+    -- vim.g.vimtex_view_general_options_latexmk = '-reuse-instance'
   end,
 }
+
+--vim.cmd("let g:vimtex_view_method = 'zathura'")
